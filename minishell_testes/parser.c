158774776571634e8e219c2c1d8dc033dfea0f8b
marcocslima <marcocslima@gmail.com>
2022-08-22 08:59:29 -0300

    mcl

--- minishell_testes/parser.c
+++ minishell_testes/parser.c
@@ -48,6 +48,63 @@ void quotes(t_data **data)
 	}
 }
 */
+
+xxxxxxx(){
+    int i,j;
+    int a[5]={1,4,8,9,11};
+    int b[6]={3,6,7,10,15,25};
+    int c[11];
+    int aux;
+
+    /* Copia vetor 'a' para a primeira metade do vetor 'c' */
+    memcpy( c, a, sizeof(a) );
+
+    /* Copia vetor 'b' para a segunda metade do vetor 'c' */
+    memcpy( c + 5, b, sizeof(b) );
+
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+        if(c[i] < c[j]){
+           //nesta parte que ocorre a troca e ordenação de varíaveis
+           aux = c[i]; //recebe o menor valor, na varável temporária aux
+           c[i] = c[j]; // repete com todos os valores no loop
+           c[j] = aux; // recebe em ordem crescente no vetor c[j]
+
+        }
+      }
+    }
+
+    for(i=0;i<10;i++){
+        printf("%d\n",c[i]); //imprime os resultados em ordem crescente
+    }
+
+
+}
+
+int *slicers(t_data **data, int n)
+{
+	int *slc;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	slc = ft_calloc(n,sizeof(int));
+	while(i < n)
+	{
+		if((*data)->tokens[0][i] < (*data)->tokens[1][j])
+		{
+			slc[i] = (*data)->tokens[0][i];
+			i++;
+		}
+		else
+		{
+			slc[i] = (*data)->tokens[1][j];
+			j++;
+		}
+	}
+}
+
 void slices(t_data **data)
 {
 	int i;
@@ -55,12 +112,14 @@ void slices(t_data **data)
 	int counter;
 	char c;
 	int flag;
+	int len;
 
 	i = 0;
 	k = 0;
-	if((*data)->len_tokens[0] > 0)
+	if((*data)->len_tokens[0] > 0 || (*data)->len_tokens[1] > 0)
 	{
-		while(i < (*data)->len_tokens[0])
+		len = (*data)->len_tokens[0] + (*data)->len_tokens[1] > 0;
+		while(i < len)
 		{
 			flag = 0;
 			counter = 0;
@@ -98,7 +157,7 @@ void parser(t_data	**data)
 
 	i = -1;
 	(*data)->tokens = malloc(sizeof(size_t) * 7);
-	(*data)->len_tokens = calloc(7,sizeof(int));
+	(*data)->len_tokens = ft_calloc(7,sizeof(int));
 	while(++i < 7)
 		get_token(data, token[i], i);
 	slices(data);
