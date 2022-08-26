4a7437b86581aa613336d77a332518985540b412
marcocslima <marcocslima@gmail.com>
2022-08-26 10:52:25 -0300

    mcl 26/8

--- minishell_testes/parser.c
+++ minishell_testes/parser.c
@@ -35,27 +35,49 @@ typedef struct s_cursors
 	int		j;
 	int		k;
 	char	c;
+	char	q;
 	int		counter;
 	int		flag;
 } t_cursors;
 
-void	init_cursors(t_cursors	**cursor)
+void	reset_conters(t_cursors	**cursor)
 {
 	(*cursor)->counter = 0;
 	(*cursor)->flag = 0;
+}
+
+void	init_cursors(t_cursors	**cursor)
+{
+	reset_conters(cursor);
 	(*cursor)->i = 0;
 	(*cursor)->j = 0;
 	(*cursor)->k = 0;
+}
+
+void	get_types(t_data **data)
+{
+	int	i;
+	int	len;
+
+	i = -1;
+	len = 0;
+	while(++i < (int)ft_strlen((*data)->input))
+		if((*data)->slicers[i] > 0)
+			len++;
+	ft_putstr_fd("\n",1);
+	ft_putnbr_fd(len,1);
+	ft_putstr_fd("\n",1);
 }
 
 void	get_slicers(t_data **data, t_cursors **cursor, char slc, int index)
 {
 	while((*cursor)->i < (*data)->len_tokens[index])
 	{
-		(*cursor)->flag = 0;
-		(*cursor)->counter = 0;
+		reset_conters(cursor);
 		while((*cursor)->k < (*data)->tokens[index][(*cursor)->i])
 		{
+			if(((*data)->input[(*cursor)->k] == '\'' || (*data)->input[(*cursor)->k] == '"'))
+				(*cursor)->q = (*data)->input[(*cursor)->k];
 			if(((*data)->input[(*cursor)->k] == '\'' || (*data)->input[(*cursor)->k] == '"') && (*cursor)->flag == 0)
 			{
 				(*cursor)->c = (*data)->input[(*cursor)->k];
@@ -63,8 +85,12 @@ void	get_slicers(t_data **data, t_cursors **cursor, char slc, int index)
 			}
 			if((*data)->input[(*cursor)->k] == (*cursor)->c)
 				(*cursor)->counter++;
-			if((*cursor)->counter % 2 == 0 && ((*data)->input[(*cursor)->k + 1] == slc))
+			if((*cursor)->counter == 2 && ((*data)->input[(*cursor)->k + 1] == slc))
+			{
 				(*data)->slicers[(*cursor)->k] = (*data)->tokens[index][(*cursor)->i];
+				(*data)->slicers_types[(*cursor)->k] = (*cursor)->q;
+				reset_conters(cursor);
+			}
 			(*cursor)->k++;
 		}
 	(*cursor)->i++;
@@ -86,7 +112,7 @@ void parser(t_data	**data)
 	while(++i < 7)
 		get_token(data, token[i], i);
 	(*data)->slicers = ft_calloc(ft_strlen((*data)->input),sizeof(int));
-	(*data)->quotes_type = ft_calloc(ft_strlen((*data)->input),sizeof(int));
+	(*data)->slicers_types = ft_calloc(ft_strlen((*data)->input),sizeof(int));
 	cursor = malloc(sizeof(t_cursors));
 	while(++s < 2)
 	{
@@ -97,12 +123,28 @@ void parser(t_data	**data)
 
 	// PARA TESTES*******************************************
 	i = 0;
-	while(i < (int)ft_strlen((*data)->input))
-		ft_putnbr_fd((*data)->slicers[i++],1);
 	ft_putstr_fd("\n",1);
+	ft_putstr_fd("Posição dos separadores: ", 1);
+	while(i < (int)ft_strlen((*data)->input))
+	{
+		if((*data)->slicers[i] == 0)
+			ft_putstr_fd("*", 1);
+		else
+			ft_putnbr_fd((*data)->slicers[i],1);
+		i++;
+	}
 	i = 0;
-	while(i < (int)ft_strlen((*data)->input))
-		ft_putnbr_fd((*data)->quotes_type[i++],1);
 	ft_putstr_fd("\n",1);
+	ft_putstr_fd("Tipos de aspas: ", 1);
+	while(i < (int)ft_strlen((*data)->input))
+	{
+		if((*data)->slicers_types[i] == 0)
+			ft_putstr_fd("*", 1);
+		else
+			ft_putnbr_fd((*data)->slicers_types[i],1);
+		i++;
+	}
+	ft_putstr_fd("\n\n",1);
+
 	// PARA TESTES*******************************************
 }
