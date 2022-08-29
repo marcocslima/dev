1846b026f33aa114d712930f16cc7b5750d4ab54
marcocslima <marcocslima@gmail.com>
2022-08-29 13:22:33 +0200

    29/8 8:22

--- minishell_testes/parser.c
+++ minishell_testes/parser.c
@@ -34,6 +34,7 @@ typedef struct s_cursors
 	int		i;
 	int		j;
 	int		k;
+	int		r;
 	int		l;
 	int		m;
 	int		begin;
@@ -46,81 +47,124 @@ typedef struct s_cursors
 	int		len;
 }	t_cursors;
 
-void	reset_conters(t_cursors	*cursor)
+void	reset_conters(t_cursors	**cursor)
 {
-	cursor->counter = 0;
-	cursor->flag = 0;
+	(*cursor)->counter = 0;
+	(*cursor)->flag = 0;
 }
 
-void	init_cursors(t_cursors	*cursor)
+void	init_crs(t_cursors	**cursor)
 {
-	reset_conters(cursor);
-	cursor->i 		= 0;
-	cursor->j 		= 0;
-	cursor->k 		= 0;
-	cursor->l 		= 0;
-	cursor->m 		= -1;
-	cursor->begin	= 0;
-	cursor->last	= 0;
-	cursor->c		= '\0';
-	cursor->q		= '\0';
-	cursor->h		= '\0';
-	cursor->len		= 0;
+	(*cursor) = (t_cursors *)malloc(sizeof(t_cursors));
+	(*cursor)->counter	= 0;
+	(*cursor)->flag		= 0;
+	(*cursor)->i 		= 0;
+	(*cursor)->j 		= 0;
+	(*cursor)->k 		= 0;
+	(*cursor)->r 		= 0;
+	(*cursor)->l 		= -1;
+	(*cursor)->m 		= -1;
+	(*cursor)->begin	= 0;
+	(*cursor)->last		= 0;
+	(*cursor)->c		= '\0';
+	(*cursor)->q		= '\0';
+	(*cursor)->h		= '\0';
+	(*cursor)->len		= 0;
 }
 
-void	len_limits(t_cursors *crs, char **st_cmds, int n)
+void teste(t_data ** data)
 {
-	while (crs->i < crs->len)
+	t_cursors *c;
+
+
+	init_crs(&c);
+	while((*data)->cmds[c->i])
 	{
-		if(st_cmds[n][crs->i] == '\'' && crs->flag == 0)
-		{
-			crs->q = '\'';
-			crs->flag = 1;
-			crs->begin = crs->i;
-		}
-		else if(st_cmds[n][crs->i] == '"' && crs->flag == 0)
-		{
-			crs->q = '"';
-			crs->flag = 1;
-			crs->begin = crs->i;
-		}
-		if (crs->q)
-			crs->last = ft_findrchr(st_cmds[n], crs->q);
-		crs->i++;
+		while((*data)->cmds[++c->l])
+			while((*data)->cmds[++c->m])
+				ft_putstr_fd((*data)->cmds[c->l][c->m], 1);
+		c->i++;
 	}
 }
 
-void get_params(t_data ** data, char **st_cmds, int n)
+void	len_limits(t_cursors **crs, char **st_cmds, int n)
+{
+	while ((*crs)->i < (*crs)->len)
+	{
+		if(st_cmds[n][(*crs)->i] == '\'' && (*crs)->flag == 0)
+		{
+			(*crs)->q = '\'';
+			(*crs)->flag = 1;
+			(*crs)->begin = (*crs)->i;
+		}
+		else if(st_cmds[n][(*crs)->i] == '"' && (*crs)->flag == 0)
+		{
+			(*crs)->q = '"';
+			(*crs)->flag = 1;
+			(*crs)->begin = (*crs)->i;
+		}
+		if ((*crs)->q)
+			(*crs)->last = ft_findrchr(st_cmds[n], (*crs)->q);
+		(*crs)->i++;
+	}
+}
+
+void	ver(char *scr)
 {
 	t_cursors	*crs;
 
-	crs = malloc(sizeof(t_cursors));
-	init_cursors(crs);
-	crs->len = ft_strlen(st_cmds[n]);
-	len_limits(crs, st_cmds, n);
+	init_crs(&crs);
+	while(scr[crs->i])
+	{
+		ft_putchar_fd(scr[crs->i], 1);
+		crs->i++;
+	}
+	ft_putchar_fd('\n', 1);
+	while(scr[crs->j])
+	{
+		ft_putnbr_fd(scr[crs->j], 1);
+		ft_putchar_fd(' ', 1);
+		crs->j++;
+	}
+	ft_putchar_fd('\n', 1);
+	free(crs);
+}
+
+void get_params(t_data ** data, char *st_cmd, int n)
+{
+	t_cursors	*crs;
+
+	init_crs(&crs);
+	crs->len = ft_strlen(st_cmd);
+	len_limits(&crs, &st_cmd, n);
 	while (crs->m++ < crs->len)
 	{
+
 		if(crs->m > crs->begin && crs->m < crs->last)
-			if(st_cmds[n][crs->m] == ' ')
-				st_cmds[n][crs->m] = 1;
+			if(st_cmd[crs->m] == ' ')
+				st_cmd[crs->m] = 1;
 	}
-	(*data)->params = ft_split(st_cmds[n], ' ');
-	while((*data)->params[crs->l] != NULL)
+	ver(st_cmd);
+	(*data)->params = ft_split(st_cmd, ' ');
+	while((*data)->params[crs->r] != NULL)
 	{
-		while((*data)->params[crs->l][crs->k])
-			if((*data)->params[crs->l][crs->k++] == 1)
-				(*data)->params[crs->l][crs->k] = ' ';
+
+		while((*data)->params[crs->r][crs->k])
+			if((*data)->params[crs->r][crs->k++] == 1)
+				(*data)->params[crs->r][crs->k] = ' ';
 		crs->k = 0;
-		crs->l++;
+		crs->r++;
 	}
+	ver((*data)->params[1]);
 	(*data)->cmds[n] = (*data)->params;
 	free(crs);
+	//teste(data);
 }
 
 
 void get_cmds(t_data ** data, t_cursors *cursor)
 {
-	init_cursors(cursor);
+	init_crs(&cursor);
 	cursor->len = ft_strlen((*data)->input);
 	while (cursor->i < cursor->len)
 	{
@@ -139,8 +183,8 @@ void get_cmds(t_data ** data, t_cursors *cursor)
 	while (cursor->k < cursor->counter + 1)
 		(*data)->cmds[cursor->k++] = malloc(sizeof(size_t));
 	(*data)->st_cmds = ft_split((*data)->input, 1);
-	while (cursor->l < cursor->counter + 1)
-		get_params(data, (*data)->st_cmds, cursor->l++);
+	while (cursor->r < cursor->counter + 1)
+		get_params(data, (*data)->st_cmds[cursor->r], cursor->r++);
 	free(cursor);
 }
 
@@ -148,7 +192,7 @@ void	get_slicers(t_data **data, t_cursors *cursor, char slc, int index)
 {
 	while(cursor->i < (*data)->len_tokens[index])
 	{
-		reset_conters(cursor);
+		reset_conters(&cursor);
 		while(cursor->k < (*data)->tokens[index][cursor->i])
 		{
 			if(((*data)->input[cursor->k] == '\'' || (*data)->input[cursor->k] == '"'))
@@ -164,12 +208,32 @@ void	get_slicers(t_data **data, t_cursors *cursor, char slc, int index)
 			{
 				(*data)->slicers[cursor->k] = (*data)->tokens[index][cursor->i];
 				(*data)->slicers_types[cursor->k] = cursor->q;
-				reset_conters(cursor);
+				reset_conters(&cursor);
 			}
 			cursor->k++;
 		}
 	cursor->i++;
 	}
+}
+
+void	norm_input(t_data ** data)
+{
+	t_cursors	*crs;
+
+	init_crs(&crs);
+	while((*data)->input[crs->i])
+	{
+		ft_putchar_fd((*data)->input[crs->i], 1);
+		crs->i++;
+	}
+	ft_putchar_fd('\n', 1);
+	while((*data)->input[crs->j])
+	{
+		ft_putnbr_fd((*data)->input[crs->j], 1);
+		ft_putchar_fd(' ', 1);
+		crs->j++;
+	}
+	free(crs);
 }
 
 void parser(t_data	**data)
@@ -188,10 +252,10 @@ void parser(t_data	**data)
 		get_token(data, token[i], i);
 	(*data)->slicers = ft_calloc(ft_strlen((*data)->input),sizeof(int));
 	(*data)->slicers_types = ft_calloc(ft_strlen((*data)->input),sizeof(int));
-	cursor = malloc(sizeof(t_cursors));
+	//norm_input(data);
 	while(++s < 1)
 	{
-		init_cursors(cursor);
+		init_crs(&cursor);
 		get_slicers(data, cursor, slicers[s], s);
 	}
 	get_cmds(data, cursor);
